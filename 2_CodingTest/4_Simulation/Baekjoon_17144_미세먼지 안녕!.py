62bd522c3eadbb6b88672928c330e4a96183b1cf
dev.appendCBangJ <dev.appendCBangJ@gmail.com>
2023-06-20 09:32:51 +0900

    Review Simulation Problems

--- 2_CodingTest/4_Simulation/Baekjoon_17144_미세먼지 안녕!.py
+++ 2_CodingTest/4_Simulation/Baekjoon_17144_미세먼지 안녕!.py
@@ -14,7 +14,7 @@ for r in range(R):
 # t초 순회
 for _ in range(T):
     # 확산
-    spread_board = [[0 for _ in range(C)] for _ in range(R)] # for문 돌때마다 초기화 시켜주지 않으면 이전 시간에서의 spread값이 남아있음!!!
+    spread_board = [[0 for _ in range(C)] for _ in range(R)] # for문 돌때마다 초기화 시켜주지 않으면 이전 시간에서의 spread값이 남아있음!
     for r in range(R):
         for c in range(C):
             if board[r][c] > 0:
@@ -29,7 +29,7 @@ for _ in range(T):
     # print("1", board)
 
     # 공기 청정
-    up_dir = [[-1, 0, 0, 0], [0, 1, 0, C-1], [1, 0, machine_row-1, C-1], [0, -1, machine_row-1, 0]] # 방향 바꾸는 지점을 요렇게 안하고, 확산에서 사용한 것 처럼 부등호로 나타내도 됨!!!
+    up_dir = [[-1, 0, 0, 0], [0, 1, 0, C-1], [1, 0, machine_row-1, C-1], [0, -1, machine_row-1, 0]] # 방향 바꾸는 지점을 요렇게 안하고, 확산에서 사용한 것 처럼 부등호로 나타내도 됨!
     down_dir = [[1, 0, R-1, 0], [0, 1, R-1, C-1], [-1, 0, machine_row, C-1], [0, -1, machine_row, 0]]
     machine_coor_dir = [[(machine_row-1)-1, 0, up_dir], [machine_row+1, 0, down_dir]]
 
@@ -39,7 +39,7 @@ for _ in range(T):
         else:
             board[machine_row+1][0] = 0
         for dir_r, dir_c, last_r, last_c in dir:
-            while r != last_r or c != last_c: # and로 하면 둘 중에 하나라도 만족하면 while문 빠져나와버림. 둘 다 만족해야 빠져나오게 하려면 or 조건을 써줘야함. and를 쓸거면 수식 2개를 묶어줘야함!!!
+            while r != last_r or c != last_c: # and로 하면 둘 중에 하나라도 만족하면 while문 빠져나와버림. 둘 다 만족해야 빠져나오게 하려면 or 조건을 써줘야함. and를 쓸거면 수식 2개를 묶어줘야함!
                 tmp_r, tmp_c = r + dir_r, c + dir_c
                 board[r][c] = board[tmp_r][tmp_c]
                 r, c = tmp_r, tmp_c
@@ -54,7 +54,7 @@ for _ in range(T):
 print(sum([sum(b) for b in board])+2)
 
 # ---------------------------------------------------
-# 다른 사람 풀이 -> 비슷함
+# 다른 사람 풀이 -> 풀이 방식 같음
 # ---------------------------------------------------
 """
 import sys
--- 2_CodingTest/4_Simulation/Baekjoon_20056_마법사 상어와 파이어볼.py
+++ 2_CodingTest/4_Simulation/Baekjoon_20056_마법사 상어와 파이어볼.py
@@ -74,3 +74,85 @@ for r, c in fireball:
     for board_rc in board[r][c]:
         mass_count += board_rc[0]
 print(mass_count)
+
+# ---------------------------------------------------
+# 다른 사람 풀이 -> 풀이 방식 같음
+# ---------------------------------------------------
+"""
+from collections import defaultdict
+
+N, M, K = map(int, input().split())
+fireballs = defaultdict(list)
+for i in range(M):
+    r, c, m, s, d = map(int, input().split())
+    fireballs[(r - 1, c - 1)].append((m, s, d))  # fireballs[(r, c)] : [(m, s, d), (m, s, d)...]
+
+dy8 = (-1, -1, 0, 1, 1, 1, 0, -1)  # 8방향
+dx8 = (0, 1, 1, 1, 0, -1, -1, -1)  # 8방향
+
+
+def move_fireballs():
+    # 파이어볼 이동
+    global fireballs
+    new_fireballs = defaultdict(list)  # 이동 후의 파이어볼 정보
+    for loc, info_list in fireballs.items():
+        sy, sx = loc
+        for m, s, d in info_list:
+            ny = (sy + dy8[d] * s) % N  # d방향으로 s만큼 이동 후 N과 나머지 연산을 해줌
+            nx = (sx + dx8[d] * s) % N  # d방향으로 s만큼 이동 후 N과 나머지 연산을 해줌
+            new_fireballs[(ny, nx)].append((m, s, d))
+
+    fireballs = new_fireballs.copy()
+
+
+def all_odd_or_even(dirs):
+    # 방향이 모두 홀수 혹은 짝수인 경우 True, 아닌 경우 False 반환
+    odd_flag, even_flag = False, False
+    for d in dirs:
+        if d % 2 == 1:
+            odd_flag = True  # 홀수 발견
+        if d % 2 == 0:
+            even_flag = True  # 짝수 발견
+
+    if odd_flag and even_flag:  # 홀/짝 모두 발견된 경우 False 반환
+        return False
+    return True
+
+
+def change_duplicate_fireballs():
+    # 좌표가 중복된 파이어볼 처리
+    global fireballs
+    new_fireballs = defaultdict(list)  # 처리후의 파이어볼 정보
+
+    for loc, info_ilst in fireballs.items():
+        if len(info_ilst) == 1:  # 해당 좌표에 파이어볼이 1개인 경우
+            new_fireballs[loc].append(info_ilst[0])
+            continue
+
+        # 파이어볼이 중복된 경우
+        sum_m, sum_s, dirs = 0, 0, []  # 질량합, 속도합, 방향리스트
+        for m, s, d in info_ilst:
+            sum_m += m
+            sum_s += s
+            dirs.append(d)
+        new_m = int(sum_m / 5)  # 새로운 파이어볼 질량
+        if new_m == 0:  # 질량이 0인 경우 소멸되므로 continue
+            continue
+        new_s = int(sum_s / len(info_ilst))  # 새로운 파이어볼 속도
+        new_dirs = [0, 2, 4, 6] if all_odd_or_even(dirs) else [1, 3, 5, 7]  # 새로운 파이어볼 방향(all_odd_or_even() 함수의 결과에 따름)
+        for new_d in new_dirs:
+            new_fireballs[loc].append((new_m, new_s, new_d))
+
+    fireballs = new_fireballs.copy()
+
+
+for _ in range(K):  # k번 반복
+    move_fireballs()
+    change_duplicate_fireballs()
+
+result = 0
+for loc, info_list in fireballs.items():  # 파이어볼 리스트에서 질량합을 구함
+    for m, s, d in info_list:
+        result += m
+print(result)
+"""
